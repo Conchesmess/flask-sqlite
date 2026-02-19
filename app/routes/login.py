a97b7189796a700adb3758eaa786c84af8c57471
Steve <steve@conches.org>
2026-02-19 07:21:27 -0800

    remove pycache files, add secretBLANK

--- app/routes/login.py
+++ app/routes/login.py
@@ -27,7 +27,7 @@ oauth = OAuth(app)
 
 google = oauth.register(
     name='google',
-    client_id=client_id,  # Get client ID from environment
+    client_id=client_id,  # Get client ID from secret.py file
     client_secret=client_secret,  # Get client secret
     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={'scope': 'openid email profile'}  # Ask for email and profile info
---
+++ app/routes/secretBLANK.py
@@ -0,0 +1,6 @@
+# TODO customize these values for OUSD student projects
+# How to get these values
+# https://www.balbooa.com/help/gridbox-documentation/integrations/other/google-client-id
+
+client_id = "YourGoogleClientId"
+client_secret = "YourGoogleClientSecret"
